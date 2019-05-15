ada1668f8c7e05fd197aa902ba82f6e481075f7b
Clifton Ireri <irericlifton@yahoo.com>
2019-05-15 19:53:08 +0300

    initial commit

---
+++ datasim/DataSim.com/index.php
@@ -0,0 +1,330 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>DataSim</title>
+  </head>
+  <body>
+	  <div class="main_wrapper">
+		  <header>
+			<div class="menu_holder">
+				<div class="container">
+					<div class="top_menu">
+						<ul>
+							<li><a href="#">FAQs</a></li>
+							<li><a href="#">COMMUNITY</a></li>
+							<li><a href="#">CONTACT US</a></li>
+							<li><i class="fa fa-user"></i><a href="#">LOGIN</a></li>
+						</ul>
+					</div>
+				</div>
+				
+				<nav class="navbar navbar-expand-lg navbar-light bg-faded">
+					<div class="container nav_items">
+					  <a class="navbar-brand" href="#"><img src="images/logofinal.png"></a>
+					  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					  </button>
+
+					  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav ml-auto">
+						  <li class="nav-item active">
+							<a class="nav-link" id="what_is_datasim" href="#">What is DataSim <span class="sr-only">(current)</span></a>
+						  </li>
+						  <li class="nav-item">
+							<a class="nav-link li_items" href="#">Plans</a>
+						  </li>
+						  <li class="nav-item">
+							<a class="nav-link li_items" href="#">Coverage</a>
+						  </li>
+						  <li class="nav-item">
+							<a class="nav-link li_items" href="#">Support</a>
+						  </li>
+						</ul>
+					  </div>
+					 </div>
+				</nav>
+				<div class="container">
+					<div class="buttons-text">
+						<div class="row">
+							<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+								<div class="text_holder d-flex flex-column">
+									<span class="custom_plans" >Custom data plans</span>
+									<span class="all_your_needs">for all your needs</span>
+								</div>
+								
+								<div class="d-flex flex-row mt-5">
+									<div>
+										<button class="btn btn-block button1">Get Started<i class="fa fa-arrow-circle-right"></i></button>
+									</div>
+									<div>
+										<button class="btn btn-block button2">Buy/Activate SIM<i class="fa fa-arrow-circle-right"></i></button>
+									</div>
+								</div>
+								
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
+			
+		  </header>
+		 <div class="inner_wrapper">
+			<div>
+				<img class="img-fluid phone_image" src="images/samsung.png">	
+			</div>
+		 </div>
+		 <div class="data_sim_wrapper">
+			<div class="whats_datasim">
+				<div class="row">
+					<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+						<div class="first_panel">
+							<div class="text_holder d-flex flex-column">
+								<span class="custom_plans" >What is</span>
+								<span class="all_your_needs">Data<span class="sim">Sim</span></span>
+								<span class="text-muted">
+									 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+									 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 	
+								</span>
+							</div>
+							<div class="d-flex flex-row mt-2">
+								<div>
+									<button class="btn btn-block dsmbutton1">View Plans<i class="fa fa-arrow-circle-right"></i></button>
+								</div>
+								<div>
+									<button class="btn btn-block dsmbutton2">View Coverage<i class="fa fa-arrow-circle-right"></i></button>
+								</div>
+							</div>
+						</div>	
+						<div class="data_plan">
+							<div class ="data_plans"><span class="data">Data</span>Plans</div>
+							<p class="data_paragraph text-muted">Choose any plan You want</p>
+							<p><span class="guaranteed text-muted">Guaranteed</span></p>
+						</div>
+					</div>
+					<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6 second_panel">
+						<img class="samsung_lower" src="images/iphone.png">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="">
+		<div class="tab_holder">
+				<ul class="tabs group">
+					<li rel="panel1" class="active">7 day Plan</li> 
+					<li rel="panel2">#30 dayy Plan</li> 
+					<li rel="panel3">90 day plan</li>
+				</ul>
+			<div class="tab_info">
+				<div id="panel1" class="panel active">
+					<h2>London</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div id="panel2" class="panel">
+					<h2>New York</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+				<div id="panel3" class="panel">
+					<h2>Chicago</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+							 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+							 ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+							 nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="search-box">
+			<input type="text" class="search_box" placeholder="Search.." name="search">
+			<button type="submit" class="submit_button"><i class="fa fa-search"></i></button>
+		</div>	
+		<div class="maps">
+			<div id="map"></div>
+		</div>
+		
+		<div class="final_wrapper">
+			<div class="first">
+				<div class="row">
+					<div class="col-lg-12 col-xxl-12 col-md-12 col-sm-12 col-xxs-12">
+						<h2 class="text-center big_text">It all starts with <span class="data-sim">Data</span>sim</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+						
+					</div>
+					<div class="col-lg-3 col-xxl-3 col-md-3 col-sm-3 col-xxs-3 mt-2">
+						<span class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</span>
+					</div>
+					<div class="col-lg-3 col-xxl-3 col-md-3 col-sm-3 col-xxs-3">
+						
+					</div>
+				</div>
+				<div class="floating_image">
+					<img class="img-fluid" src="images/mobile.png">
+				</div>
+			</div>
+			<div class="second">
+				<div class="card_holder">
+					<div class="row">
+						<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+						
+						</div>
+						<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+							<form class="form" role="form" autocomplete="off" id="loginForm" novalidate="" method="POST">
+								<div class="form-row">
+									<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+										<div class="form-group">
+											<label for="firstname">First Name</label>
+											<input type="text" class="form-control" name="firstname" id="firstname" placeholder="First Name" required="">
+										</div>
+									</div>
+									<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+										<div class="form-group">
+											<label for="second">Second Name</label>
+											<input type="text" class="form-control" name="secondname" id="secondname" placeholder="Second Name" required="">
+										</div>
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+										<div class="form-group">
+											<label for="email">Email</label>
+											<input type="email" class="form-control" name="email" id="email" placeholder="Email" required="">
+										</div>
+									</div>
+									<div class="col-lg-6 col-xxl-6 col-md-6 col-sm-6 col-xxs-6">
+										<div class="form-group">
+											<label for="number">Phone Number</label>
+											<input type="text" class="form-control" name="number" id="number" placeholder="Phone Number" required="">
+										</div>
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="col-lg-12 col-xxl-12 col-md-12 col-sm-12 col-xxs-12">
+										<div class="form-group">
+											<label for="email">Email</label>
+											<input type="email" class="form-control" name="email" id="email" placeholder="Email" required="">
+										</div>
+									</div>
+								</div>
+								<button class="smbt_btn" type="submit">Submit</button>
+							</form>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+			
+			<footer>
+				<div class="">
+					<div class="row">
+						<div class="col-lg-3 col-xxl-3 col-md-3 col-sm-3 col-xxs-3">
+							<ul class="mt-5">
+								<li><a href="#">COMMUNITY</a></li>
+								<li><a href="#">CONTACT US</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-3 col-xxl-3 col-md-3 col-sm-3 col-xxs-3">
+							<ul>
+								<li class="mb-2"><strong><a href="#">SUPPORT</a></strong></li>
+								<li><a href="#">PRODUCT SUPPORT</a></li>
+								<li><a href="#">CHAT SUPPORT</a></li>
+								<li><a href="#">FAQs</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-3 col-xxl-3 col-md-3 col-sm-3 col-xxs-3">
+							<ul>
+								<li class="mb-2"><strong><a href="#">ACCOUNT</a></strong></li>
+								<li><a href="#">MY ACCOUNT</a></li>
+								<li><a href="#">MY RENEWALS</a></li>
+								<li><a href="#">GET A SIM</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-3 col-xxl-3 col-md-3 col-sm-3 col-xxs-3">
+							<ul>
+								<li class="mb-2"><strong><a href="#">RESOURCES</a></strong></li>
+								<li><a href="#">MY ACCOUNT</a></li>
+								<li><a href="#">MY RENEWALS</a></li>
+								<li><a href="#">GET A SIM</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<div class="social">
+				<ul>
+					<li><i class="fa fa-facebook-f"></i></li>
+					<li><i class="fa fa-twitter"></i></li>
+					<li><i class="fa fa-youtube"></i></li>
+				</ul>
+			</div>
+		</div>
+		<div class=" container copyright">
+			<div class="row">
+				<div class="col">
+					<p class="text-center">Copyright&copy;2018|All Rights Reserved.</p>
+				</div>
+			</div>
+		</div>
+	  </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDC2f4eG-jDdYelIcfPa5EaF2L74_W1OL4&callback=initMap"></script>
+	<script>
+		$(function(){
+			$('.tab_holder ul li').on('click',function(){
+				var toShow=$(this).attr('rel');
+				$('.tab_holder ul li.active').removeClass('active');
+				$(this).addClass('active');
+				$('.tab_holder .panel.active').slideUp(400,function(){
+					$(this).removeClass('active');
+					$('#'+toShow).slideDown(400,function(){
+						$(this).addClass('active');
+					});
+				});
+				
+			});
+		});
+	</script>
+	<script>
+      function initMap() {
+        var options={
+			zoom:8,
+			center:{lat:-0.5803771999999999,lng:37.3225307}
+		}
+		var map=new google.maps.Map(document.getElementById('map'),options);
+		
+		var marker=new google.maps.Marker({
+			position:{lat:-0.5803771999999999,lng:37.3225307},
+			map:map,
+			icon:''
+		});
+      }
+    </script>
+  </body>
+</html>
